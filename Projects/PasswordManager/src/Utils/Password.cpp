4c008c48cbd1905e8c4d68ace5839cb0cfdaa8bb
Astatine387 <270602688+Astatine387@users.noreply.github.com>
2026-05-14 03:07:06 -0700

    Apply Google C++ Style Guide

--- Projects/PasswordManager/src/Utils/Password.cpp
+++ Projects/PasswordManager/src/Utils/Password.cpp
@@ -12,11 +12,11 @@
 
 bool Password::Equal(const Password& other) const {
   volatile uint8_t diff = (size_ != other.size_) ? 1 : 0;
-  size_t min_size_ = (size_ < other.size_) ? size_ : other.size_;
+  size_t min_size = (size_ < other.size_) ? size_ : other.size_;
 
   for (size_t i = 0; i < kMaxPWLen; i++) {
-    uint8_t a = (i >= min_size_) ? 0 : static_cast<uint8_t>(data_[i]);
-    uint8_t b = (i >= min_size_) ? 0 : static_cast<uint8_t>(other.data_[i]);
+    uint8_t a = (i >= min_size) ? 0 : static_cast<uint8_t>(data_[i]);
+    uint8_t b = (i >= min_size) ? 0 : static_cast<uint8_t>(other.data_[i]);
 
     diff |= a ^ b;
   }
