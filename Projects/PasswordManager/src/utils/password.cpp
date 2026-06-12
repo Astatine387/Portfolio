5200876b6a41bec664fd1b69f89235fe7cc0fd17
Astatine387 <270602688+Astatine387@users.noreply.github.com>
2026-06-12 09:33:38 -0700

    Remove password length dependency on Password::Equal

--- Projects/PasswordManager/src/utils/password.cpp
+++ Projects/PasswordManager/src/utils/password.cpp
@@ -12,11 +12,10 @@
 
 bool Password::Equal(const Password& other) const {
   volatile uint8_t diff = (size_ != other.size_) ? 1 : 0;
-  size_t min_size = (size_ < other.size_) ? size_ : other.size_;
 
   for (size_t i = 0; i < kMaxPWLen; i++) {
-    uint8_t a = (i >= min_size) ? 0 : static_cast<uint8_t>(data_[i]);
-    uint8_t b = (i >= min_size) ? 0 : static_cast<uint8_t>(other.data_[i]);
+    uint8_t a = (data_ != nullptr) ? static_cast<uint8_t>(data_[i]) : 0;
+    uint8_t b = (other.data_ != nullptr) ? static_cast<uint8_t>(other.data_[i]) : 0;
 
     diff |= a ^ b;
   }
@@ -53,12 +52,11 @@ Result Password::SetData(const char* str, size_t len) {
 
   if (str) {
     size_ = len;
-    data_ = new char[size_ + 1];
 
-    locked_ = (Lock(data_, size_ + 1) == Result::kSuccess);
+    data_ = new char[kMaxPWLen + 1]{};
+
+    locked_ = (Lock(data_, kMaxPWLen + 1) == Result::kSuccess);
     memcpy(data_, str, size_);
-
-    data_[size_] = '\0';
   }
 
   return Result::kSuccess;
@@ -66,8 +64,8 @@ Result Password::SetData(const char* str, size_t len) {
 
 void Password::Clean() {
   if (data_ != nullptr) {
-    Wipe(data_, size_ + 1);
-    Unlock(data_, size_ + 1);
+    Wipe(data_, kMaxPWLen + 1);
+    Unlock(data_, kMaxPWLen + 1);
 
     delete[] data_;
   }
