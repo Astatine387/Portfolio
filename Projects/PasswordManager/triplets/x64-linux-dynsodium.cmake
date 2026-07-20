b1b207423b876ca36a46350f867803a8b50d9f46
Astatine387 <270602688+Astatine387@users.noreply.github.com>
2026-07-20 09:18:48 -0700

    Fix BLAKE2b collision

---
+++ Projects/PasswordManager/triplets/x64-linux-dynsodium.cmake
@@ -0,0 +1,10 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+if(PORT STREQUAL "libsodium")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+    set(VCPKG_FIXUP_ELF_RPATH ON)
+endif()
